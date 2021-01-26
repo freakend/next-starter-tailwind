a6d16ce4d9e7396a7f7ff5ab8585cb7c2d187d67
Freakend <freakend.fr@gmail.com>
2021-01-27 01:56:14 +0700

    little update, moved project section to about.js and make index.js into single page, its been 2 am btw i need sleep

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <div className="flex-col items-left justify-center space-y-12">
+    <div className="flex-col items-left justify-center space-y-12 md:px-28">
         <div className="md:flex md:justify-center flex justify-center mb-6">
         <Image
         alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
@@ -14,7 +14,7 @@ export default function AboutPage() {
       <div>
         <section className="mb-6">
           {/* <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2> */}
-          <p className="text-lg text-gray-800">
+          <p className="text-lg text-gray-800 md:-mb-5">
           My name is Ferdi. I'm a former IT technician who loves to code. I mostly used PHP and I'm currently learning Typescript, Javascript, and Nodejs.
           </p> <br />
           <p className="text-lg text-gray-800">
@@ -22,12 +22,12 @@ export default function AboutPage() {
           </p>
         </section>
 
-        <section>
-        <h1 className="mb-3 text-3xl font-bold">Experience</h1>
+        <section className="text-gray-800">
+        <h1 className="mb-3 text-3xl font-bold text-black">Experience</h1>
         <hr />
-        <h2 className="mb-3 text-2xl">Senior IT Support</h2>
+        <h2 className="mb-4 text-2xl">Senior IT Support</h2>
         <h3 className="mb-3 text-l -my-4 font-bold">PT Lotte Shopping Indonesia | Bogor, West Java</h3>
-        <h4 className="mb-3 text-m -my-4 font-italic">2019-2020</h4>
+        <h4 className="mb-3 md:mb-1 text-m -my-4 font-italic">2019-2020</h4>
         <p className="text-lg text-gray-800">
           As I got promoted, I lead team of three managing and handling software
           and hardware issue. We believe things get easier when you have a great
@@ -35,9 +35,9 @@ export default function AboutPage() {
         </p>
 
         <br />
-        <h2 className="mb-3 text-2xl">IT Support</h2>
+        <h2 className="mb-4 text-2xl">IT Support</h2>
         <h3 className="mb-3 text-l -my-4 font-bold">PT Lotte Shopping Indonesia | Jakarta</h3>
-        <h4 className="mb-3 text-m -my-4 font-italic">2016-2019</h4>
+        <h4 className="mb-3 md:mb-1 text-m -my-4 font-italic">2016-2019</h4>
         <p className="text-lg text-gray-800">
           I was responsible for managing software and hardware issues; from POS to
           Digital Scales, Computers to Server rack. I also managed Daily, weekly
@@ -46,7 +46,26 @@ export default function AboutPage() {
         </p>
         </section>
       </div>
-
+      <div>
+      <section class="text-gray-800 body-font">
+      <h1 className="mb-3 text-3xl font-bold text-black">Selected Project</h1>
+        <hr />
+            <div class="container px-1 mx-auto py-1 lg:px-4">
+                <div class="flex flex-wrap text-left">
+                    <div class="px-2 py-6 lg:w-1/3 md:w-full">
+                        <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                            src="/rumahtek.png" alt="content"></img>
+                        <div class="flex items-center justify-between ">
+                            <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> Rumah Teknologi Air
+                            </h2>
+                            <span class="mb-1 text-xs font-medium tracking-widest text-blue-500 title-font">2017</span>
+                        </div>
+                        <p class="mb-4 text-base leading-relaxed">I lead team of three, our main idea was simple; creating a product where everyone, especially they who are in needs, can discuss about their problem with experts at Direktorat Jenderal Sumber Daya Air. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+      </div>
       
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,19 @@ export default function IndexPage() {
   return (
     <div className="flex flex-col items-left justify-center space-y-12">
       
-      {/* <Image
-        src="/team-of-critters.svg"
-        alt="Four one-eyed aliens playing"
-        width={25}
-        height={25}
-        priority
-      />  */}
-      
-      {/* <h2 className="font-bold text-3xl -mb-10">
-        Hi, I'm Ferdiansyah Rahman
-      </h2>
-
-      <p className="-mt-5">Former IT Support, I'm a born problem solver, writer, and gamer. This is my personal website where i will mostly write and build things. Currently learning javascript and typescript.
-      </p> */}
+        <div className="md:flex md:justify-center md:pt-1 flex pt-20 justify-center">
+            <Image
+            alt="welcome"
+            src="/undrawhelo.svg"
+            width={250}
+            height={250}
+        />
+        </div>
 
         <section class="text-gray-700 body-font">
-            <div class="container px-2 pt-24 md:py-20 mx-auto lg:px-4">
+            <div class="container px-2 pt-0 md:py-2 mx-auto lg:px-4">
                 <div class="flex flex-col w-full mb-12 text-left lg:text-center">
-                    <h2 class="mb-1 text-s font-semibold tracking-widest text-black-600 uppercase title-font">Hi!</h2>
+                    <h2 class="mb-1 text-s font-semibold tracking-widest text-black-600 italic title-font">Welcome!</h2>
                     <h1 class="mb-1 text-2xl font-semibold tracking-tighter text-black-800 sm:text-5xl title-font">
                         I'm Ferdiansyah Rahman
                     </h1>
@@ -32,55 +26,6 @@ export default function IndexPage() {
                 </div>
                 
               </div>
-        </section>
-
-      <h2 className="mb-2 text-2xl font-semibold tracking-tighter text-gray-700 sm:text-4xl title-font mx-auto flex flex-col">
-        Selected Project
-      </h2>
-      <hr/>
-      <section class="text-gray-700 body-font">
-            <div class="container px-1 mx-auto py-1 lg:px-4">
-                <div class="flex flex-wrap text-left">
-                    <div class="px-2 py-6 lg:w-1/3 md:w-full">
-                        <img class="object-cover object-center w-full h-40 mb-6 rounded"
-                            src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="content"></img>
-                        <div class="flex items-center justify-between ">
-                            <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> Rumah Teknologi Air
-                            </h2>
-                            <span class="mb-1 text-xs font-medium tracking-widest text-blue-500 title-font">2017</span>
-                        </div>
-                        <p class="mb-4 text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                    <div class="px-2 py-6 lg:w-1/3 md:w-full">
-                        <img class="object-cover object-center w-full h-40 mb-6 rounded"
-                            src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="content"></img>
-                        <div class="flex items-center justify-between ">
-                            <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> Information 2
-                            </h2>
-                            <span class="mb-1 text-xs font-medium tracking-widest text-blue-500 title-font">Your
-                                tagline</span>
-                        </div>
-                        <p class="mb-4 text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                    <div class="px-2 py-6 lg:w-1/3 md:w-full">
-                        <img class="object-cover object-center w-full h-40 mb-6 rounded"
-                            src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="content"></img>
-                        <div class="flex items-center justify-between ">
-                            <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> Information 3
-                            </h2>
-                            <span class="mb-1 text-xs font-medium tracking-widest text-blue-500 title-font">Your
-                                tagline</span>
-                        </div>
-                        <p class="mb-4 text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-            </div>
         </section>
                                     
 
