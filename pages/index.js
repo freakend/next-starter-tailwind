03756152dd27b34b4eb61433d23ef0b67a25c6dc
Ferdiansyah Rahman <44191555+freakend@users.noreply.github.com>
2021-10-18 12:52:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,28 +21,10 @@ export default function IndexPage() {
 				/>
 			</Head>
 
-        <div className="md:flex md:justify-center md:pt-1 flex pt-20 justify-center">
-            <Image
-            alt="welcome"
-            src="/undrawhelo.svg"
-            width={250}
-            height={250}
-        />
-        </div>
+
 
         <section class="text-gray-700 body-font">
-            <div class="container px-2 pt-0 md:py-2 mx-auto lg:px-4">
-                <div class="flex flex-col w-full mb-12 text-left lg:text-center">
-                    <h2 class="mb-1 text-s font-semibold tracking-widest text-black-600 italic title-font">Welcome!</h2>
-                    <h1 class="mb-1 text-2xl font-semibold tracking-tighter text-black-800 sm:text-5xl title-font">
-                        I'm Ferdiansyah Rahman
-                    </h1>
-                    <p class="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-2/3">
-                    a born problem solver, writer, and gamer. This is my personal website where i will mostly write and build things.
-                    </p>
-                </div>
-                
-              </div>
+            <p>under construction</p>
         </section>
                                     
 
